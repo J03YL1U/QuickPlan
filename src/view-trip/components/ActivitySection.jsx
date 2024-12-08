10f9b22ef1fb3d280cfd2db8aeae2d8432f879be
Joey <joeyliuliongwah@gmail.com>
2024-12-08 15:35:23 -0500

    check if itinerary is an array or not

--- src/view-trip/components/ActivitySection.jsx
+++ src/view-trip/components/ActivitySection.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import PlaceCard from "./PlaceCard";
 
 function ActivitySection({ trip }) {
+
+  const itinerary = Array.isArray(trip.TripData?.itinerary)
+  ? trip.TripData.itinerary
+  : trip.TripData?.itinerary
+  ? [trip.TripData.itinerary]
+  : [];
+
   return (
     <div>
       <h2 className="font-bold text-lg">Places to Visit</h2>
-
       <div>
-        {trip.TripData?.itinerary?.map((activity, index) => (
+        {itinerary.map((activity, index) => (
           <div className="mt-5">
             <h2 className="font-medium text-lg">Day {activity?.day}</h2>
             <div className="grid grid-cols-2 gap-5">
